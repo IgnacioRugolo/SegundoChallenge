1327e8f8d440819cf95f6d5ec0d9a9e8aec7f86b
Ignacio Rugolo <tomasignaciorugolo@gmail.com>
2022-01-18 03:12:03 -0300

    Declaracion de variables constantes y eventos

---
+++ index.js
@@ -0,0 +1,9 @@
+const share = document.getElementById('divClic') 
+const funcion= document.getElementById('cuadroGris')
+
+
+share.addEventListener('click', function(){
+funcion.setAttribute("style", "display:block;")
+})
+
+
