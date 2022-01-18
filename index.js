b60375db2a19d573b150112cf4754375f93eacf9
Ignacio Rugolo <tomasignaciorugolo@gmail.com>
2022-01-18 04:44:45 -0300

    Finalizacion de sector JS

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const share = document.getElementById('divClic') 
 const funcion= document.getElementById('cuadroGris')
-
+const segundaFuncion = document.getElementById('divSegundoShare')
 
 share.addEventListener('click', function(){
 funcion.setAttribute("style", "display:block;")
+})
+
+
+segundaFuncion.addEventListener('click', function(){
+    funcion.setAttribute("style", "display:none;")
 })
 
 
