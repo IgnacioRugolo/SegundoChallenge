be2359fbe7c5016ac1521bcdc862fc8f31a15273
Ignacio Rugolo <tomasignaciorugolo@gmail.com>
2022-01-25 16:10:21 -0300

    Finalizacion de challenge, con agregado de detalle en cuadro gris y media querie

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const share = document.getElementById('divClic') 
 const funcion= document.getElementById('cuadroGris')
 const segundaFuncion = document.getElementById('divSegundoShare')
+const click= document.getElementById('click')
 
-share.addEventListener('click', function(){
-funcion.setAttribute("style", "display:block;")
+share.addEventListener("click", function (){
+
+funcion.classList.toggle("clicked")
+if(funcion.classList == "cuadroGris clicked")
+    funcion.setAttribute ("style", "display:block")
+  click.setAttribute("style", "fill: #ffffff !important;")  
+}
+)
+
+
+share.addEventListener("click", function(){
+    if(funcion.classList == "cuadroGris")
+    funcion.setAttribute ("style", "display:none")
+
 })
 
 
 segundaFuncion.addEventListener('click', function(){
+    funcion.classList.toggle("clicked")
     funcion.setAttribute("style", "display:none;")
+    
+  
 })
+
+
 
 
